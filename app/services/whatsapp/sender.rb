498de5849b5a3f4b0f378da8faec94a377af8764
Tomasz Ociepa <mangi@MacBook-Air-Tomasz.local>
2025-12-18 00:15:52 +0100

    deploy railway config url

--- app/services/whatsapp/sender.rb
+++ app/services/whatsapp/sender.rb
@@ -1,8 +1,9 @@
 module Whatsapp
     class Sender
       def self.call(chat_id, message)
-        uri = URI("#{ENV.fetch('WHATSAPP_GATEWAY_URL')}/send")
-  
+        # uri = URI("#{ENV.fetch('WHATSAPP_GATEWAY_URL')}/send")
+        uri = URI.join(ENV.fetch("WHATSAPP_GATEWAY_URL").strip, "/send")
+
         Net::HTTP.post(
           uri,
           { to: chat_id, message: message }.to_json,
